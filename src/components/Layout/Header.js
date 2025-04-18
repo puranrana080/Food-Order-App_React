0536dd37d78c0b98035223abd8e9e3638546f1b7
Puran Singh Rana <puransinghrana080@gmail.com>
2025-04-18 22:39:30 +0530

    build header of food order app

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,18 @@
+import HeaderCartButton from "./HeaderCartButton";
+import mealImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealImage} alt="a table with lots of food" />
+      </div>
+    </>
+  );
+};
+export default Header;
